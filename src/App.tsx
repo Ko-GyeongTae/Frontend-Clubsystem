8e53de13186b4645684f2d276fae354300183e22
Ko-GyeongTae <kokt0203@naver.com>
2021-12-16 22:44:47 +0900

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <div className="header">
+        <h1>동아리 관리시스템</h1>
+        <p>Made by 맛소금</p>
+      </div>
+
+      <div>
+        
+      </div>
+    </main>
   );
 }
 
